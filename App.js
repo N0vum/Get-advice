690c1f307cfab412bb9165a6ecf7f42bc2478873
Elena Kazakova <61557802+N0vum@users.noreply.github.com>
2022-03-23 12:41:48 +0700

    Update App.js
    created function get(); removed stringify; removed {useEffect}

--- App.js
+++ App.js
@@ -1,17 +1,18 @@
-import React, {useState, useEffect} from 'react'
+import React, {useState} from 'react'
 
 
 function App() {
   const [text, setText] = useState()
-
-
-
+  function get(){
+    fetch('https://api.adviceslip.com/advice')
+    .then(response => response.json())
+    .then(json =>  setText(json.slip.advice))
+  }
+  
   return (
   <div className='container'>
-  <div className='field'>{JSON.stringify(text.slip.advice, null, 2)}</div>
-  <button className='button' onClick={()=>{fetch('https://api.adviceslip.com/advice')
-.then(response => response.json())
-.then(json => setText(json))}}>get an advice</button>
+  <div className='field'>{text}</div>
+  <button className='btn' onClick={get}>get an advice</button>
     </div>
   );
 }
